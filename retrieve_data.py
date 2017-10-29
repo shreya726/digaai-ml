12efd8d248c9a1975404e7f36e3cebbd9a060877
Ben Gaudiosi <bgaudiosi531@gmail.com>
2017-10-29 15:40:46 -0400

    Data retrieval tool

---
+++ retrieve_data.py
@@ -0,0 +1,37 @@
+key = "EAACEdEose0cBACCMEIVSPPVBZCafVABc9eswlvrka0D5LPLoZB6NTZAYTgw8GDnudjX0zuYyKjkmYo2tfGAGC5mBxuJZChBynHtaWvLVRWK7wMBK4R9AxFzhI10n2vBuvIXyZBKN8h4ZBctjZABe6wRtPRXtMAl4bZCxKV9wPJELD7ObNWR2EIZAfYfIU4QmlieQZD"
+
+
+import http.client
+import json
+import csv
+
+def get_names(filename, group_ids):
+    f = open(filename, "w")
+    writer = csv.writer(f, quoting=csv.QUOTE_NONE)
+    
+    for group_id in group_ids:
+        conn = http.client.HTTPSConnection("graph.facebook.com")
+        next_link = " "
+        while not next_link == None:
+            names = []
+            conn.request("GET", "/v2.10/" + group_id + "/members?access_token=" + key + "&fields=name&after=" + next_link)
+            res = conn.getresponse()
+            data = res.read()
+            json_data = json.loads(data)
+        
+            for user in json_data["data"]:
+                name = [user["name"]]
+                writer.writerow(name)
+
+            if "paging" in json_data:
+                next_link = str(json_data["paging"]["cursors"]["after"])
+            else:
+                next_link = None
+    
+    f.close()
+
+brazil_ids = ["422121401312686"]
+portugal_ids = []
+
+get_names("brazilians.csv", brazil_ids)
+get_names("portuguese.csv", portugal_ids)
