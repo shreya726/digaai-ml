b9ba7914d46c7884cf971971c50563dd59f70e98
wjdan94 <walharth@bu.edu>
2017-10-30 23:53:52 -0400

    started reading data func

--- model.py
+++ model.py
@@ -10,7 +10,10 @@ TRAN_STEPS = 1000
 BATCH_SIZE = 0
 
 MAX_NAME_LENGTH = 10
-CSV_COLUMNS = ['first', 'last']
+
+# TODO @Ben all column names
+CSV_COLUMNS = ['first?', 'last?']
+LABEL_COLUMN = 'first?'
 
 def save_grams():
     """
@@ -19,9 +22,56 @@ def save_grams():
     pass
 
 def read_dataset(mode):
+    # use mode to create filename
+    filename = "PATH OF FILE"
+    if prefix == "train":
+        mode = tf.contrib.learn.ModeKeys.TRAIN
+    else:
+        mode = tf.contrib.learn.ModeKeys.EVAL
 
     def _input_fn()   # gets passed to tensorflow
-        pass
+        input_file_names = tf.train.match_filenames_once(filename)
+        filename_queue = tf.train.string_input_producer(input_file_names, shuffle=True)
+
+        # load the data
+        reader = tf.TextLineReader()
+        _, value = reader.read_up_to(filename_queue, num_records=BATCH_SIZE)
+        value_column = tf.expand_dims(value, -1)
+
+        # TODO @Ben add record_defaults and field_delim values
+        # read https://www.tensorflow.org/api_docs/python/tf/decode_csv
+        columns = tf.decode_csv(value_column, record_defaults=NONE, field_delim=NONE)
+        features = dict(zip(CSV_COLUMNS, columns))
+        label = feature.pop(LABEL_COLUMN)
+
+        # convert input to numeric value
+        # TODO @Duaa and @Shreya: adjust the code below so it does sth like this
+        # 1- looks up the 3-gram's value in the 3-gram's mapping
+        # 2- if doesn't exist, construct one using the letter mapping
+        # 3- and adds it to the table, anything else?
+
+        #table = tf.contrib.lookup.index_table_from_tensor(mapping=tf.contant(TARGETS))
+        #target = table.lookup(label)
+
+        # first it creates a table with required params
+        # read https://www.tensorflow.org/api_docs/python/tf/contrib/lookup/index_table_from_file
+        from sys import maxsize as NULL
+        table = tf.contrib.lookup.index_table_from_file(
+                vocabulary_file='DATA FILENAME', num_oov_buckets=NULL,
+                vocab_size=NULL default_value=NULL)
+
+        # tf.constant just converts the name split (3-grams) into a "constant" type for lookup
+        # read https://www.tensorflow.org/api_docs/python/tf/constant
+        # replace NULL with 3-grams of current input
+        numbers = table.lookup(tf.constant(NULL))
+
+        # this code initializes a table and keeps it open for other iterations
+        # read here: https://www.tensorflow.org/api_docs/python/tf/tables_initializer
+        with tf.Session() as sess:
+            tf.tables_initializer().run()
+            print "{} --> {}".format(lines[0], numbers.eval())
+            
+        return features, target
 
     return _input_fn
 
