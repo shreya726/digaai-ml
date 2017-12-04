330c7d083d34ed3e863a24e499496e08f5b343ce
shreya726 <shreya@bu.edu>
2017-12-04 16:03:51 -0500

    WOOO model working

--- model.py
+++ model.py
@@ -1,15 +1,16 @@
 #!/usr/bin/env python
 
+import csv
+import json
+
 import tensorflow as tf
 import tensorflow.contrib.learn as tflearn
 import tensorflow.contrib.metrics as metrics
 from tensorflow.contrib import lookup
 from tensorflow.python import debug as tf_debug
-import json
-import csv
-import pandas as pd
 
 from mapping import mapping as MAPPING
+
 with open('grams.json') as data:
 	GRAMS = json.load(data)
 
@@ -17,13 +18,11 @@ tf.logging.set_verbosity(tf.logging.INFO)
 
 TRAIN_STEPS = 20
 
-# TODO @Shreya
 BATCH_SIZE = 1000
 
 MAX_NAME_LENGTH = 10
 PADDING = 'ZYXW'
 
-# TODO @Ben all column names
 CSV_COLUMNS = ['first1','first2','first3','first4','first5','first6','first7','first8','first9','first10','last1', 'last2', 'last3', 'last4', 'last5', 'last6', 'last7', 'last8', 'last9', 'last10','source']
 LABEL_COLUMN = 'source'
 
@@ -31,8 +30,6 @@ LABEL_COLUMN = 'source'
 CLASSES = ['1', '0']  
 
 def read_dataset(mode):
-	# TODO @Ben use mode to create filename so that we can 
-	# pass mode as 'train' or 'eval'
 	filename = "./data/"	#PATH OF FILE"
 
 
@@ -44,70 +41,49 @@ def read_dataset(mode):
 		filename += "eval_processed.csv" # FIXME for testing?
 
 	def _input_fn():   # gets passed to tensorflow
-		# gets the file and parses it
 		
+		# Get file names
 		input_file_names = tf.train.match_filenames_once(filename)
 		filename_queue = tf.train.string_input_producer(input_file_names, shuffle=True)
-		#df = pd.read_csv(filename,header=None)
 		
 		# load the data with given batch size (constant above)
 		reader = tf.TextLineReader(skip_header_lines=False)
 		_, value = reader.read_up_to(filename_queue, num_records=BATCH_SIZE)
 		value_column = tf.expand_dims(value, -1)
 
-		# TODO @Ben add record_defaults and field_delim values
-		# read https://www.tensorflow.org/api_docs/python/tf/decode_csv
+		# Read CSV files
 		columns = tf.decode_csv(value_column, record_defaults=[['first1'],['first2'],['first3'],['first4'],['first5'],['first6'],['first7'],['first8'],['first9'],['first10'],['last1'], ['last2'], ['last3'], ['last4'], ['last5'], ['last6'], ['last7'], ['last8'], ['last9'], ['last10'],['source']], field_delim=',')
-		#features = dict(zip(CSV_COLUMNS, columns))
-		#print(columns)
+
 		tf.logging.info('after decode_csv')
-		features = tf.string_to_number(tf.slice(columns, [0,0,0], [20,900,1]), tf.float32)
-		label = tf.string_to_number(tf.slice(columns,[0,21,0],[1,900,1]), tf.float32)
-		tf.logging.info('after slicing columns')
-		#with tf.Session() as sess:
-		#	sess = tf_debug.LocalCLIDebugWrapperSession(sess)
-		#	sess.run(features)  # to print a tensor must start a sessions
-		tf.logging.info(features)	
+
+		# Converting features and labels to numbers (decoded from CSV as string)
+		features = tf.string_to_number(columns[0:20], tf.float32)
+		label = tf.string_to_number(columns[20:21], tf.float32)
+
+		# Reshaping features and labels
+		features = tf.reshape(features,shape=[-1,20])
+		label = tf.reshape(label, shape=[-1,1])
+	
 		return features, label
-	tf.logging.info('right before returning _input_fn')
-	#x, y = _input_fn()
-	#tf.logging.info(x)
+	
 	return _input_fn
 
 def cnn_model(features, target, mode):
-	# load the 3-gram mappings 
-	# TODO @Shreya for filename 
-	#table = lookup.index_table_from_file(vocabulary_file="grams.json",
-	#		num_oov_buckets=1, default_value=-1)
-	
-	# where CSV_COLUMNS[0] is 'first'
-	# I'm not sure if the axis argument is useful in our case 
-	# sice our input has only one value in it. So I remove axis=[1]
-	# read https://www.tensorflow.org/api_docs/python/tf/squeeze
-	# but I am also converned that tf.squeeze() will remove the name itself
-	# so I will mark this FIXME in case we run into problems later
-	#print(features)
-	#print(target)
 	tf.logging.info('inside cnn_model')
 	first_name = features
-	#last_name = tf.squeeze(input=features[CSV_COLUMNS[1]])
-	#with tf.Session() as sess:
-	#	tf.logging.info(first_name.eval())	
-	#	sess.close()
-	#tf.logging.info(str(len(first_name)))
-	logits = tf.contrib.layers.fully_connected(first_name, num_outputs=2, activation_fn=None)
-	tf.logging.info(type(logits))
-	# TODO figure out the ethniticity (source) part 
+
+	logits = tf.contrib.layers.fully_connected(first_name, num_outputs=1, activation_fn=None)
+
 	predictions_dict = {
 			'ethnicity' : tf.gather(CLASSES, tf.argmax(logits, 1)),
 			'class' : tf.argmax(logits, 1),
 			'prob' : tf.nn.softmax(logits)
 	}
-	tf.logging.info(str(predictions_dict['class']))
-	#tf.logging.info(len(target.value))
+
 	if mode == tf.contrib.learn.ModeKeys.TRAIN or \
 			mode == tf.contrib.learn.ModeKeys.EVAL:
-		loss = tf.losses.sparse_softmax_cross_entropy(tf.cast(target, tf.int32), logits)
+		loss = tf.nn.softmax_cross_entropy_with_logits(labels=target, logits=logits)
+		loss = tf.reduce_mean(loss)
 		train_op = tf.contrib.layers.optimize_loss(
 				loss,
 				tf.contrib.framework.get_global_step(),
@@ -124,7 +100,6 @@ def cnn_model(features, target, mode):
 			train_op=train_op)
 
 def serving_input_fn():
-	#'last'  : tf.placeholder(tf.string, [None]),
 	feature_placeholders = {
 			'first' : tf.placeholder(tf.string, [None]),
 	}
@@ -140,13 +115,9 @@ def serving_input_fn():
 
 def get_train():
 	return read_dataset('train')
-	#tf.logging.info('inside get_train')
-	#return x,y
 
 def get_validate():
 	return read_dataset('eval')
-	#tf.logging.info('inside get_validate')
-	#return x,y
 
 from tensorflow.contrib.learn.python.learn.utils import saved_model_export_utils
 def train_fn(output_dir):
--- submission.sh
+++ submission.sh
@@ -1,27 +1,8 @@
-#!/bin/bash -l
-
-#Specify project
-#$ -P cs542
-
-#Request appropriate time (default 12 hours; gpu jobs time limit - 2 days (48 hours), cpu jobs - 30 days (720 hours) )
-#$ -l h_rt=12:00:00
-
-#Send an email when the job is done or aborted (by default no email is sent)
-#$ -m e
-
-# Give job a name
-#$ -N hello
-
-#$ Join output and error streams into one file
-#$ -j y
-
-
-
 #load appropriate envornment
 module load cuda/8.0
 module load cudnn/5.1
-module load python/3.6.0
-module load tensorflow/r1.0_python-3.6.0
+module load python/3.6.2
+module load tensorflow/r1.3_cpu
 
 #execute the program
 python task.py
